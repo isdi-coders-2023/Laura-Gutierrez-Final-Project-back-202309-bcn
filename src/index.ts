30bcf07c7a5d86a58c34b1763037f49551c7d04a
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-27 15:33:23 +0100

    Add orchestra director

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-// Director de orquesta 🦥
+import chalk from "chalk";
+import { startServer } from "./server/app";
+import { connectToDatabase } from "./database";
+
+const port = process.env.PORT ?? 4000;
+if (!process.env.MONGODB_URL) {
+  console.log(chalk.red("Missing MongoDB connection string"));
+  process.exit();
+}
+
+const mongoUrl = process.env.MONGODB_URL;
+
+await connectToDatabase(mongoUrl);
+
+startServer(+port);
