11ba6235c4ee4e5668bc2c935dd8efe611a4a20b
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-28 13:41:55 +0100

    Add ping router supertest

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import app from "./app.js";
 import cors from "cors";
 import morgan from "morgan";
+import { pingRouter } from "../features/ping/router/pingRouter.js";
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -11,3 +12,4 @@ const corsPort = process.env.ALLOWED_ORIGIN;
 const corsOption = { origin: corsPort };
 
 app.use(cors(corsOption));
+app.use("/", pingRouter);
