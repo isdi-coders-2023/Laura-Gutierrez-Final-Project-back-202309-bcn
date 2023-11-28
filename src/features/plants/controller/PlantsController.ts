ac9722164292c9b0ac7a9b3246b0ba69b63a2609
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-28 17:37:46 +0100

    Add PlantsController

---
+++ src/features/plants/controller/PlantsController.ts
@@ -0,0 +1,12 @@
+import { type Request, type Response } from "express";
+import type PlantsMongooseRepository from "../repository/PlantsMongooseRepository";
+
+class PlantsController {
+  constructor(private readonly plantsRepository: PlantsMongooseRepository) {}
+
+  public getPlants = async (_req: Request, res: Response): Promise<void> => {
+    const plants = await this.plantsRepository.getPlants();
+
+    res.status(200).json({ plants });
+  };
+}
