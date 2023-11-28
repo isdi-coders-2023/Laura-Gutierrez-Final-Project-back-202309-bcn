7648f21ffcbb76ad797327f310a4591b5a6a7217
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-28 09:44:01 +0100

    Install cors and add corsOption

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,9 @@
 import express from "express";
 import app from "./app.js";
+import cors from "cors";
 
+const corsPort = process.env.ALLOWED_ORIGIN;
+const corsOption = { origin: corsPort };
+
+app.use(cors(corsOption));
 app.use(express.json());
