4cd362a18b9a947e0c7a6495d3a44716b4b9a6a7
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-28 11:55:27 +0100

    Add notFound test to __tests__ folder

---
+++ src/server/middlewares/errors/__tests__/notFound.test.ts
@@ -0,0 +1,24 @@
+import type { Request, Response, NextFunction } from "express";
+import CustomError from "../../../customError/CustomError";
+import { notFoundError } from "../errorMiddleware";
+
+describe("Given a notFound function", () => {
+  describe("When it receives a NextFunction", () => {
+    test("Then it should call the next function with a customError", () => {
+      const req = {};
+      const res = {};
+      const next: NextFunction = jest.fn();
+
+      const customError = new CustomError(
+        "Endpoint not found",
+        404,
+        "errorMiddlewWare:notFound",
+        "testPrivateMessage",
+      );
+
+      notFoundError(req as Request, res as Response, next);
+
+      expect(next).toHaveBeenCalledWith(customError);
+    });
+  });
+});
