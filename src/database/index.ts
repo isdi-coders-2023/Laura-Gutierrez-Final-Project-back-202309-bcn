f0284df7863d68532258bb2eb310f2e77ec2590c
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-28 10:49:35 +0100

    Add morgan into configurations

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,7 @@ import chalk from "chalk";
 import mongoose from "mongoose";
 import debugCreator from "debug";
 
-const debug = debugCreator(":root:src:index");
+const debug = debugCreator("root:database:connectToDatabase");
 
 export const connectToDatabase = async (mongoUrl: string) => {
   try {
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,13 @@
+import "dotenv/config";
 import express from "express";
 import app from "./app.js";
 import cors from "cors";
+import morgan from "morgan";
+
+app.use(morgan("dev"));
+app.use(express.json());
 
 const corsPort = process.env.ALLOWED_ORIGIN;
 const corsOption = { origin: corsPort };
 
 app.use(cors(corsOption));
-app.use(express.json());
