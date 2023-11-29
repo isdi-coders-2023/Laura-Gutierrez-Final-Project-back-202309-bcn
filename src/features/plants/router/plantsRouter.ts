02855a4a593d58a0debeb08d03f0ecac4351d89c
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-11-29 06:18:19 +0100

    Add plants router

---
+++ src/features/plants/router/plantsRouter.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import PlantsController from "../controller/PlantsController.js";
+import PlantsMongooseRepository from "../repository/PlantsMongooseRepository.js";
+
+const plantsRouter = Router();
+
+const plantsRepository = new PlantsMongooseRepository();
+const plantsController = new PlantsController(plantsRepository);
+
+plantsRouter.get("/", plantsController.getPlants);
+
+export default plantsRouter;
