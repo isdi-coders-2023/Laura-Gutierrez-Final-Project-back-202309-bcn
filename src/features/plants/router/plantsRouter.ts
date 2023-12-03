ae93e0ba40456bdf6536093aa9cfc734054a045e
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-03 20:06:00 +0100

    Add getPlantsById into router

--- src/features/plants/router/plantsRouter.ts
+++ src/features/plants/router/plantsRouter.ts
@@ -8,5 +8,5 @@ const plantsRepository = new PlantsMongooseRepository();
 const plantsController = new PlantsController(plantsRepository);
 
 plantsRouter.get("/", plantsController.getPlants);
-
+plantsRouter.get("/:id", plantsController.getPlantsById);
 export default plantsRouter;
