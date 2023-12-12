1a939ee308336e61ba9e767e200b30012de2464c
Laura Gutierrez <lauragsanahuja@gmail.com>
2023-12-12 10:39:31 +0100

    Add new mock into mocks folder

---
+++ src/features/plants/mocks/modifiedPlantMock.ts
@@ -0,0 +1,15 @@
+import { type PlantStructure } from "../types";
+
+export const modifiedPlantMock: PlantStructure = {
+  _id: "6566158cd11a3f8f1075c7a1",
+  name: "Oregano",
+  scientificName: "Origanum vulgare",
+  use: "Culinary/Medicinal",
+  properties:
+    "Relieves headaches and throat aches. It's a natural constipation remedy. Relieves cough and asthma symptoms.",
+  howToUse:
+    "Use it in many savoury dishes as seasoning, also in sauces. You can consume oregano oil in capsules. ",
+  imageUrl: "https://www.image.com/fake-one",
+  isPoisonous: "false",
+  habitat: "Grasslands, bushes, rocky areas.",
+};
